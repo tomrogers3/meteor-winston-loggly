87ac23ce1134d778d53a3b99350921f9eb15358c
Tom Rogers <tomrogers3@gmail.com>
2013-08-19 23:17:41 -0400

    updated to support Meteor 0.6.5

--- package.js
+++ package.js
@@ -3,10 +3,13 @@ Package.describe({
 });
 
 Npm.depends({
-    "winston": "0.7.1",
+    "winston": "0.7.2",
     "winston-loggly": "0.6.0"
 });
 
 Package.on_use(function (api, where) {
   api.add_files('winston-loggly.js', 'server');
+  if(api.export){
+    api.export('Winston_Loggly');
+  }
 });
